34fd45936af6289ac54667cd3300e171c93aa385
Khai Truong <kminht@gmail.com>
2022-04-19 22:20:29 -0400

    test if poions will return a random name

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,9 +1,18 @@
+const { expect } = require('@jest/globals');
 const Potion = require('../lib/Potion.js');
 
-
+// creates a new potion object
 test('creates a health potion object', () => {
     const potion = new Potion('health');
 
     expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
 });
