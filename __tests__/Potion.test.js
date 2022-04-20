e1ec220b1176cd781fe2ed2cbf5f9ff2ae1dd38b
Khai Truong <kminht@gmail.com>
2022-04-19 22:00:21 -0400

    potion test pass

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -5,5 +5,5 @@ test('creates a health potion object', () => {
     const potion = new Potion('health');
 
     expect(potion.name).toBe('health');
-    expect(potion.value).toEqual(expect.any(number));
+    expect(potion.value).toEqual(expect.any(Number));
 });
