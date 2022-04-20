53cc24c7b255aa9dacd29be9145f924f5b55bf1c
Khai Truong <kminht@gmail.com>
2022-04-19 21:20:27 -0400

    test code

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,10 +1,16 @@
-const { expect } = require("@jest/globals");
-const { test } = require("jest-circus");
-const checkIfEqual = require("../lib/random");
+const randomNumber = require('../lib/random.js');
+
+
+// describe('check if 10 is equal to 10', ()=> {
+//     it('should be true', () => {
+//         expect(checkIfEqual(10, 10).toBe(true));
+//     });
+// });
 
 
 
 
-test("checks if 10 is equal to 10", () => {
-  expect(checkIfEqual(10, 10)).toBe(true);
-});
+test('gets random number between 1 and 10', ()=> {
+    expect(randomNumber()).toBeGreaterThanOrEqual(1);
+    expect(randomNumber()).toBeLessThanOrEqual(10);
+})
