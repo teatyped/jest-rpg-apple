0b15a5211c8b7b616ad09fe1ff964698021eba32
Khai Truong <kminht@gmail.com>
2022-04-21 14:05:37 -0400

    exepct inventory to fail

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,4 @@
-const { expect } = require("@jest/globals");
+const { expect, test } = require("@jest/globals");
 const Player = require("../lib/Player");
 const Potion = require("../lib/Potion");
 
@@ -19,5 +19,23 @@ test("creates a player object", () => {
   );
 
   console.log(player);
-
 });
+
+test('gets player stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+})
