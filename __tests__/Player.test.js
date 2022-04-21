d0a08d8519d4c8ba998c8c49c095465d3968c21c
Khai Truong <kminht@gmail.com>
2022-04-21 13:36:04 -0400

    player object can get invetory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,17 +1,23 @@
-const Player = require('../lib/Player');
-const Potion = require('../lib/Potion');
+const { expect } = require("@jest/globals");
+const Player = require("../lib/Player");
+const Potion = require("../lib/Potion");
 
-jest.mock('../lib/Potion.js');
+jest.mock("../lib/Potion.js");
 
 console.log(new Potion());
 
+test("creates a player object", () => {
+  const player = new Player("Dave");
 
-test('creates a player object', () => {
-    
-    const player = new Player('Dave');
+  expect(player.name).toBe("Dave");
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
 
-    expect(player.name).toBe('Dave');
-    expect(player.health).toEqual(expect.any(Number));
-    expect(player.strength).toEqual(expect.any(Number));
-    expect(player.agility).toEqual(expect.any(Number));
+  expect(player.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+  );
+
+  console.log(player);
+
 });
