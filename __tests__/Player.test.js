a7f10c2aabda47b94d1e725abfc543827287db2a
Khai Truong <kminht@gmail.com>
2022-04-22 13:37:52 -0400

    testing getplayer heatlh method

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -21,21 +21,32 @@ test("creates a player object", () => {
   console.log(player);
 });
 
-test('gets player stats as an object', () => {
-    const player = new Player('Dave');
+test("gets player stats as an object", () => {
+  const player = new Player("Dave");
 
-    expect(player.getStats()).toHaveProperty('potions');
-    expect(player.getStats()).toHaveProperty('health');
-    expect(player.getStats()).toHaveProperty('strength');
-    expect(player.getStats()).toHaveProperty('agility');
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("strength");
+  expect(player.getStats()).toHaveProperty("agility");
 });
 
-test('gets inventory from player or returns false', () => {
-    const player = new Player('Dave');
+test("gets inventory from player or returns false", () => {
+  const player = new Player("Dave");
 
-    expect(player.getInventory()).toEqual(expect.any(Array));
+  expect(player.getInventory()).toEqual(expect.any(Array));
 
-    player.inventory = [];
+  player.inventory = [];
 
-    expect(player.getInventory()).toEqual(false);
-})
+  expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player health value", () => {
+  const player = new Player("Dave");
+
+  expect(player.getHealth()).toEqual(
+    expect.stringContaining(player.health.toString())
+  );
+
+  console.log(player);
+
+});
